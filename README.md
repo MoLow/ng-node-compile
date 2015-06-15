5cf74de2ab6da0c3dc93ae12393c5e97ff86117a
MoLow <moshe@atlow.co.il>
2015-06-15 22:26:10 +0300

    some more things work now

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # ng-node-compile
-Compile html templates the angular way, in node js
+
+Compile html templates the angular way, in node js!
+
+## Install
+
+```bash
+$ npm install ng-node-compile
+```
+
+## How to Use?
+The library exposes several angular services, which will let you compile angular templates inside node:
+
+###$interpolate:
+
+```js
+    var ngcompile = require('ng-node-compile');
+    var ngEnviorment = new ngcompile();
+    ngEnviorment.$interpolate("hello {{name}}")({ name: 'Jhon doe' });
+```
+
+this wil return a string "hello Jhon doe"
+
+###$compile:
+
+```js
+    var ngcompile = require('ng-node-compile');
+    var ngEnviorment = new ngcompile();
+    ngEnviorment.$compile("<div ng-repeat=\"n in [1,2,3,4,5]\">hello {{name}} {{n}}</div>")({ name: 'Jhon doe' });
+```
+
+this wil return the following HTML:
+
+```html
+    <div ng-repeat="n in [1,2,3,4,5]" class="ng-binding ng-scope">hello Moshe 1</div>
+    <div ng-repeat="n in [1,2,3,4,5]" class="ng-binding ng-scope">hello Moshe 2</div>
+    <div ng-repeat="n in [1,2,3,4,5]" class="ng-binding ng-scope">hello Moshe 3</div>
+    <div ng-repeat="n in [1,2,3,4,5]" class="ng-binding ng-scope">hello Moshe 4</div>
+    <div ng-repeat="n in [1,2,3,4,5]" class="ng-binding ng-scope">hello Moshe 5</div>
+```
--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-﻿var jsdom = require("jsdom-nogyp"),
-    fs = require("fs");
+﻿var jsdom = require("jsdom-nogyp");
 
+var ENVIORMENT_NOT_READY = "Angular enviorment not yet ready";
 
 function ngCompile(modules, angularPath) {
     this.modules = modules || [], _self = this;
     this.modules.push(angularPath || "./public/angular.js");
+    this.ready = false;
+
 
     this.env = jsdom.env({
         html: '<p></p>',
@@ -14,33 +16,37 @@ function ngCompile(modules, angularPath) {
             else {
                 global.window = window;
                 global.document = window.document;
-                require("./public/angular.js");
+
+                _self.modules.forEach(function (module) { require(module); })
 
                 _self.window = window;
                 _self.angular = window.angular;
                 window.angular.injector(['ng']).invoke(function ($rootScope, $compile, $interpolate) {
                     _self.services = { $rootScope: $rootScope, $compile: $compile, $interpolate: $interpolate };
-
-                    console.log('ready')
+                    this.ready = true;
                 });
             }
         }
     });
 }
 ngCompile.prototype.$new = function () {
+    if (!this.ready) throw new Error(ENVIORMENT_NOT_READY);
     return this.services.$rootScope.$new()
 }
 ngCompile.prototype.$interpolate = function (html) {
+    if (!this.ready) throw new Error(ENVIORMENT_NOT_READY);
     return this.services.$interpolate(html)
 }
 ngCompile.prototype.$compile = function (html) {
+    if (!this.ready) throw new Error(ENVIORMENT_NOT_READY);
     if (typeof html === "object") html = (html.length ? html[0].outerHTML : html.outerHTML);
     var $scope = this.$new(), _self = this;
     return function (context) {
         _self.angular.extend($scope, context);
         var elem = _self.services.$compile(html)($scope);
+        elem = _self.angular.element('<div/>').append(elem);
         $scope.$apply();
-        var str = elem[0].outerHTML;
+        var str = elem[0].innerHTML;
         $scope.$destroy();
         elem = $scope = null;
         return str;
