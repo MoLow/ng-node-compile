a3c07dee90d8f4fa3b11752bfdd162511beacc0e
MoLow <moshe@atlow.co.il>
2015-06-15 21:58:47 +0300

    its starting to work

---
+++ app.js
@@ -0,0 +1,11 @@
+﻿var express = require('express');
+var app = express();
+
+var ngcompile = require('./main.js');
+
+var ngenv = new ngcompile();
+app.use(express.static('public'));
+app.get('/', function (req, res) {
+    res.send(ngenv.$compile("<div>{{name}}</div>")({ name: 'aaa' }));
+});
+app.listen(3000);
---
+++ main.js
@@ -0,0 +1,51 @@
+﻿var jsdom = require("jsdom-nogyp"),
+    fs = require("fs");
+
+
+function ngCompile(modules, angularPath) {
+    this.modules = modules || [], _self = this;
+    this.modules.push(angularPath || "./public/angular.js");
+
+    this.env = jsdom.env({
+        html: '<p></p>',
+        done: function (errors, window) {
+            if (errors)
+                console.log(errors);
+            else {
+                global.window = window;
+                global.document = window.document;
+                require("./public/angular.js");
+
+                _self.window = window;
+                _self.angular = window.angular;
+                window.angular.injector(['ng']).invoke(function ($rootScope, $compile, $interpolate) {
+                    _self.services = { $rootScope: $rootScope, $compile: $compile, $interpolate: $interpolate };
+
+                    console.log('ready')
+                });
+            }
+        }
+    });
+}
+ngCompile.prototype.$new = function () {
+    return this.services.$rootScope.$new()
+}
+ngCompile.prototype.$interpolate = function (html) {
+    return this.services.$interpolate(html)
+}
+ngCompile.prototype.$compile = function (html) {
+    if (typeof html === "object") html = (html.length ? html[0].outerHTML : html.outerHTML);
+    var $scope = this.$new(), _self = this;
+    return function (context) {
+        _self.angular.extend($scope, context);
+        var elem = _self.services.$compile(html)($scope);
+        $scope.$apply();
+        var str = elem[0].outerHTML;
+        $scope.$destroy();
+        elem = $scope = null;
+        return str;
+    }
+}
+
+
+module.exports = ngCompile;
